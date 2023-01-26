2f985088059ee0d793f7afb8a65e7669a7b857ff
Ser-AM <sergio.alvarez.monroy@gmail.com>
2023-01-26 23:19:31 +0000

    Header. Menú principal

---
+++ assets/partials/header.php
@@ -0,0 +1,14 @@
+<header>
+    <div id="contenedor-header">
+        <div class="logo">
+            <img src="logo.png" alt="Logotipo de la empresa">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#"><i class="fa-solid fa-house fa-4x"></i></a></li>
+                <li><a href="#"><i class="fa-solid fa-gear fa-4x"></i></li>
+            </ul>
+
+        </nav>
+    </div>
+</header>
--- index.php
+++ index.php
@@ -8,10 +8,12 @@
         <title id='title'>Inicio</title>
         <script src="./assets/js/jquery-3.6.0.min.js"></script>
         <script src="./assets/js/script.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     </head>
     <body>
         
         <?php session_start(); ?>
+        <?php include_once "./assets/partials/header.php"; ?>
         <div id="contenido" class="container">
             <?php 
                 if(!isset($_SESSION['logged_in']) || !$_SESSION['logged_in']) {
